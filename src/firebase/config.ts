cf3233611987a6e0f973e701d0f67fc707abcfed
MRIGANKA BHUSAN Debnath <debnathmriganka2010@gmail.com>
2026-02-25 17:43:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4318202088-27de4",
+  "appId": "1:21800149097:web:6a4bc4d54b4cd0034c82f8",
+  "apiKey": "AIzaSyCHuVOqOp4pUsHlq34bFxYivIK0615dFto",
+  "authDomain": "studio-4318202088-27de4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "21800149097"
+};
